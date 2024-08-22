95736e2aa6bbf9300176f6efca9b14fab8bb8a71
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2024-08-22 14:52:21 -0400

    Navigate to profile from retwatted notif

--- src/app/components/retwatted-by/retwatted-by.component.ts
+++ src/app/components/retwatted-by/retwatted-by.component.ts
@@ -3,6 +3,9 @@ import { map } from 'rxjs';
 import { UserFacade } from '../../features/user/user.facade';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { User } from '../../features/user/user.entity';
+import { MessageFacade } from '../../features/message/message.facade';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-retwatted-by',
@@ -15,11 +18,21 @@ export class RetwattedByComponent {
   @Input() userId?: number;
 
   userFacade: UserFacade = inject(UserFacade);
+  messageFacade: MessageFacade = inject(MessageFacade);
+  router: Router = inject(Router);
   user$;
 
   constructor() {
     this.user$ = this.userFacade.users$.pipe(
       map((users) => users.find((u) => u.id == this.userId))
     );
+  }
+
+  goToProfile(user: User) {
+    if (!user.deleted) {
+      this.messageFacade.applyFilter('twats');
+      this.userFacade.loadUser(user.id);
+      this.router.navigate(['home', 'profile', user.id]);
+    }
   }
 }
